1d5c8bd6e95d07e35b3a82fa790fb818dc73b09e
yoMarks <cristianheredia789@gmail.com>
2026-04-13 21:52:00 -0400

    rotacion nave

--- script.js
+++ script.js
@@ -3,11 +3,22 @@ const menu = document.getElementById("menu");
 const canvas = document.getElementById("juegoCanvas");
 const ctx = canvas.getContext("2d");
 
+
+const estadoTecla = {
+  izq: false,
+  der: false
+};
+
+// objeto nave, pensar si mi nave se trabajara funcinal o objetos, avanzar hasta disparos y rotacion 360(frame algo y loop)
 const nave = {
   x: canvas.width / 2,
   y: canvas.height / 2,
-  angulo: 0
+  angulo: 0,
+  velocidadRotacion: 0.06
 };
+
+//mantiene loop inactivo
+let juegoActivo = false;
 
 function pintarfondo() {
   ctx.fillStyle = "black";
@@ -31,13 +42,40 @@ function dibujarNave() {
   ctx.restore();
 }
 
-function render() {
+function actualizarNave() {
+  if (estadoTecla.izq) nave.angulo -= nave.velocidadRotacion;
+  if (estadoTecla.der) nave.angulo += nave.velocidadRotacion;
+}
+
+function loopJuego() {
+  if (!juegoActivo) return;
+
+  actualizarNave();
   pintarfondo();
   dibujarNave();
+
+  requestAnimationFrame(loopJuego); //rotacion continua, juego se repite 60veces por segundo
 }
+
+// function render() {
+//   pintarfondo();
+//   dibujarNave();
+// }
 
 iniciar.addEventListener("click", () => {
   menu.style.display = "none";
   canvas.style.display = "block";
-  render();
+  juegoActivo = true;
+  loopJuego();
+  // render();
+});
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowLeft") estadoTecla.izq = true;
+  if (e.key === "ArrowRight") estadoTecla.der = true;
+});
+
+window.addEventListener("keyup", (e) => {
+  if (e.key === "ArrowLeft") estadoTecla.izq = false;
+  if (e.key === "ArrowRight") estadoTecla.der = false;
 });
